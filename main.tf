14f4af0ce31e7b9cb3d84e11c9376ff72e0119ac
jasonbeebe <43306520+jasonbeebe@users.noreply.github.com>
2025-02-01 16:05:31 -0500

    up to speed

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+
 data "aws_ami" "app_ami" {
   most_recent = true
 
@@ -23,12 +24,12 @@ module "blog_vpc" {
   azs             = ["us-west-2a","us-west-2b","us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-
   tags = {
     Terraform = "true"
     Environment = "dev"
   }
 }
+
 
 module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
